a7f8829e145b18c1710dd43bdd8e5b35d8401f0b
Tyler Stright <tylers@nezpercetribe.org>
2018-08-13 11:15:07 -0600

    SGS close to complete

--- R/01_load_fins_data.R
+++ R/01_load_fins_data.R
@@ -14,24 +14,45 @@ library(stringr)
 
 # load FINS data.
 data <- read_excel('./data/MODIFIED UNIQUE All_Research_FINS_data_standardized.xlsx')
+# OR Tyler's
+data <- read_excel('C:/TylerS Sync/FINS Info/R_Fins/data/MODIFIED UNIQUE All_Research_FINS_data_standardized.xlsx')
 
 # load NPT Mark/Tag Protocol. label df: mt_protocol
 mt_protocol <- read_excel('./data/NPT Mark and Tag Protocol.xlsx')
+# OR Tyler's
+mt_protocol <- read_excel('C:/TylerS Sync/FINS Info/R_Fins/data/NPT Mark and Tag Protocol.xlsx')
 
 #------------------------------------------------------------------------------
-# Combine fins data with protocol and create useable dataset
+# Create dataframe for `StreamName` associated with `weir`
+#------------------------------------------------------------------------------
+weir <- c('Bradford Weir (Lolo Creek)', 'Joseph Creek Weir', 'Camp Creek Weir',
+          'Imnaha River Weir', 'Freezeout Creek Weir', 'Gumboot Creek Weir', 
+          'Dry Creek Weir', 'Upper Lolo Weir', 'Lolo Creek Weir', 
+          'Johnson Creek Weir', 'NPT Hatchery Trap', 'Lostine River Weir', 
+          'Newsome Creek Weir', 'Lower Granite Dam Trap')
+StreamName <- c('Lolo Creek', 'Joseph Creek', 'Camp Creek', 'Imnaha River', 
+                'Freezeout Creek', 'Gumboot Creek', 'Dry Creek', 'Lolo Creek', 
+                'Lolo Creek', 'Johnson Creek', 'NPT Hatchery Trap', 
+                'Lostine River', 'Newsome Creek', 'Lower Granite Dam Trap')
+streams <- data.frame(weir, StreamName)
+
+#------------------------------------------------------------------------------
+# Combine fins data with protocol, StreamName, and create useable dataset
 #------------------------------------------------------------------------------
 fins_data <- data %>%
-  mutate(Trap_Year = year(`Trapped Date`)) %>%
   separate(Trap, into = c("weir", "Trap"), sep = " - ") %>%
+  mutate(Trap_Year = year(`Trapped Date`)) %>%
   left_join(mt_protocol) %>%
+  left_join(streams) %>%
   mutate(Marks = ifelse(str_detect(`Applied Marks`, mark_type), TRUE,
                         ifelse(str_detect(`Applied Tags`, tag_type), TRUE,
                                ifelse(str_detect(`Applied PIT`, tag_type), TRUE, FALSE))))
 
+
+
 #------------------------------------------------------------------------------
-# Combine fins data with protocol and create useable dataset
+# Save modified fins data
 #------------------------------------------------------------------------------
 save(fins_data, file = 'C:/R input/fins_data.Rda')
 save(fins_data, file = './data/fins_data.Rda')
-      
+save(fins_data, file = 'C:/TylerS Sync//FINS Info/R_Fins/data/fins_data.Rda')      
---
+++ R/carcass_detail.R
@@ -0,0 +1,118 @@
+#------------------------------------------------------------------------------
+#      Chinook: Joining SGS Data with FINS Data for Mark/Recap Estimates
+# 
+#                                                ><(((*>   ><>  ~
+# Author: Tyler Stright
+# Created: 8/6/18
+#------------------------------------------------------------------------------
+
+# load packages
+library(tidyverse)
+library(RODBC)
+
+#------------------------------------------------------------------------------
+# Create connection with database
+con <- odbcConnect('sgs_master', uid = 'guest', pwd = 'guest') # named dsn in admin tools.
+#------------------------------------------------------------------------------
+#   Extract carcass database table from SQL Server via above (con)nection
+      sql_carcassdetail <- sqlFetch(con, 'carcass_detail')
+#------------------------------------------------------------------------------
+        # Save ****RAW**** Data
+          save(sql_carcassdetail, file = 'C:/TylerS Sync//FINS Info/R_Fins/data/sql_carcassdetailraw.Rda')
+#------------------------------------------------------------------------------
+# Summarize carcass_detail for captures and recaptures
+
+carcass_mod <- sql_carcassdetail  %>%
+  mutate(Trap_Year = SurveyYear)        # column for joining with 'fins_data'
+
+carcass_recaps <- carcass_mod  %>%
+  filter(Recapture == 'Yes') %>%
+  group_by(StreamName, Trap_Year) %>%
+  summarise(c_recaps = n())            
+
+carcass_caps <- carcass_mod  %>%
+  filter(Recapture == 'No') %>%
+  group_by(StreamName, Trap_Year) %>%
+  summarise(c_captures = n())            
+            
+tmp <- left_join(carcass_recaps, carcass_caps)
+
+rm(carcass_mod, carcass_recaps, carcass_caps)
+
+#------------------------------------------------------------------------------
+# Import/Summarize captures and recaptures for LOSTINE RIVER (ODFW SGS)
+
+# import lostine Mark/Tag Protocol
+lostine_protocol <- read_excel('./data/Lostine River Weir Mark and Tag Protocol.xlsx')
+# OR Tyler's
+lostine_protocol <- read_excel('C:/TylerS Sync/FINS Info/R_Fins/data/Lostine River Weir Mark and Tag Protocol.xlsx')
+#------------------------------------------------------------------------------
+# modify ODFW SGS data to have matching fields with 'fins_data' and Marking Protocol
+ODFWSGS <- rawODFWdata %>%
+  mutate(Trap_Year = Year,
+         StreamName = River) %>%
+  filter(StreamName == 'Lostine River')
+
+tmpODFWSGS <- left_join(ODFWSGS, lostine_protocol) %>%
+  mutate(Recap = ifelse(str_detect(`Opercle Punch Type`, mark_type), TRUE, FALSE))
+#Check the NA values
+
+# Summarize (ODFW) Lostine River SGS for captures and recaptures
+lostine_captures <- tmpODFWSGS %>%
+  filter(Recap == 'FALSE') %>%
+  group_by(StreamName, Trap_Year) %>%
+  summarise(c_captures = n()) 
+
+lostine_recaps <- tmpODFWSGS %>%
+  filter(Recap == 'TRUE') %>%
+  group_by(StreamName, Trap_Year) %>%
+  summarise(c_recaps = n())
+
+lostine_sgs <- left_join(lostine_captures, lostine_recaps)
+
+rm(tmpODFWSGS, lostine_captures, lostine_recaps)
+#------------------------------------------------------------------------------
+# Join NPT SGS and ODFW (Lostine) SGS summaries
+sgs_cr <- bind_rows(tmp, lostine_sgs)
+
+#------------------------------------------------------------------------------
+# Get number of upstream fish that were released with a mark
+marks <- fins_data %>%
+  filter(Species == 'Chinook') %>%
+  filter(`Moved To` == 'Upstream') %>%
+  filter(Marks == "TRUE") %>%
+  filter(Recap != 'TRUE') %>%
+  group_by(weir, StreamName, Trap_Year) %>%
+  summarise(weir.marks = sum(Count))
+
+#------------------------------------------------------------------------------
+# Get number of downstream moving fish without marks = Captures - doesn't include recaptures
+captures <- fins_data %>%
+  filter(Species == 'Chinook') %>%
+  filter(`Moved To` == 'Downstream') %>%
+  #filter(Marks == FALSE) %>% # no applied marks
+  filter(Recap == FALSE) %>% # first fish handle
+  group_by(weir, StreamName, Trap_Year) %>%
+  summarise(weir.captures = sum(Count))
+
+chinook_mc <- left_join(marks, captures)
+
+#------------------------------------------------------------------------------
+# Join summarized weir data with summarized SGS data and calculate estimates
+chinook_mcr <- left_join(chinook_mc, sgs_cr) %>%
+  select(StreamName, Trap_Year, weir.marks, weir.captures, c_recaps, c_captures) %>%
+  mutate(Captures = ifelse(is.na(c_captures), 0, c_captures),
+         n1 = weir.marks,
+         n2 = c_recaps + c_captures,
+         m2 = c_recaps,
+         Nhat = (((n1 + 1)*(n2 + 1))/(m2 + 1)) - 1,
+         Vhat = ((n1+1)*(n2+1)*(n1-m2)*(n2-m2))/((m2+1)^2*(m2+2)),
+         lower95 = Nhat - 1.96*sqrt(Vhat),
+         upper95 = Nhat + 1.96*sqrt(Vhat))
+
+#  ggplot(chinook_mcr, aes(x = Trap_Year, y = Nhat, colour = weir)) +
+#    geom_point(size = 2, position = position_dodge(width = .2)) +
+#    geom_errorbar(aes(ymin = lower95, ymax = upper95), position = position_dodge(width = .2)) +
+#    facet_wrap(~weir, scale = 'free_y', drop = TRUE) +
+#    theme_bw()
+
