d11f2e805fd37ae90b140550c44c91854db4d6bd
Tyler Stright <tylers@nezpercetribe.org>
2018-10-22 15:54:44 -0600

    Improved again!

--- R/06_npt_summary.R
+++ R/06_npt_summary.R
@@ -5,33 +5,26 @@
 # Author: Tyler Stright
 # Created: 8/8/18
 #------------------------------------------------------------------------------
-# Create list of FINS purposes for Summary.
-purpose.list <- c('Biological Sampling', 'Brood Stock', 'Distribution', 
-                  'Fisheries','General Holding', 'Natural Spawning', 
-                  'Nutrient Enhancement', 'Other', 'Recycled', 'Stray Removal', 
-                  'Stray Relocation', 'Stray Removal Distribution', 'Unknown', 
-                  'Within FINS Facility', NA)
-
 # Summarize count of fish with same "Purpose"
 npt_purpose <- fins_data %>%
-  filter(Purpose %in% purpose.list) %>%
+  filter(Purpose %in% c('Biological Sampling', 'Brood Stock', 'Distribution', 
+                        'Fisheries','General Holding', 'Natural Spawning', 
+                        'Nutrient Enhancement', 'Other', 'Recycled', 'Stray Removal', 
+                        'Stray Relocation', 'Stray Removal Distribution', 'Unknown', 
+                        'Within FINS Facility', NA)) %>%
   group_by(weir, Trap_Year, Purpose, Species, `Moved To Facility`) %>%
   summarise(count = sum(Count)) %>%
   spread (key = Purpose, value = count, fill = "")
 #------------------------------------------------------------------------------
-# Create list of MORTALITY types
-all.morts <- c('TrapMort', 'DOA', 'Killed')
-
 # Summarize count of Killed, Trap Mort, DOA and Total dead fish records
 npt_deadfish <- fins_data %>%
-  filter(`Living Status` %in% all.morts) %>%
+  filter(`Living Status` %in% c('TrapMort', 'DOA', 'Killed')) %>%
   group_by(weir, Trap_Year, `Living Status`, Species) %>%
   summarise(morts = sum(Count)) %>%
   spread(key = `Living Status`, value = morts, fill = 0 ) %>%
   select(weir, Trap_Year, Species, TrapMort, DOA, Killed) %>%
   mutate(Total_morts = TrapMort + DOA + Killed) %>%
   arrange(weir, desc(Trap_Year))
-
 
 #------------------------------------------------------------------------------
 # Summarize Out-planted fish
